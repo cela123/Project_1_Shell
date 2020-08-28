ef9f5c64f4a8d3202f669f38ca196698f3999ef4
amico <alecamico123@yahoo.com>
2020-08-28 18:15:26 -0400

    Update shell.c

--- AA_files/shell.c
+++ AA_files/shell.c
@@ -5,33 +5,31 @@
 
 int main()
 {
-	while (1) {
+	while (1) 
+	{
 		printf("%s@%s : %s > ", getenv("USER"), getenv("MACHINE"), getenv("PWD"));
-
-		/* input contains the whole command
-		 * tokens contains substrings from input split by spaces
-		 */
 
 		char *input = get_input();
 		printf("whole input: %s\n", input);
 
 		tokenlist *tokens = get_tokens(input);
-		for (int i = 0; i < tokens->size; i++) {
+		for (int i = 0; i < tokens->size; i++) 
+		{
 			printf("token %d: (%s)\n", i, tokens->items[i]);
-
-      if(*(tokens->items[i]) == '$'){
-        char tempStr[strlen(tokens->items[i])];
-
+			char tempStr[strlen(tokens->items[i])];
+      		if(*(tokens->items[i]) == '$')
+			{
+        		//char tempStr[strlen(tokens->items[i])];
 				strcpy(tempStr, tokens->items[i]);
-			  if(tempStr[0] == '$')
-					memmove(tempStr, tempStr+1, strlen(tempStr));
-
-        printf("%s dereferenced: %s\n", tempStr, getenv(tempStr));
-        printf("Tempsize is: %d\n", strlen(tempStr));
-
-
-        printf("Hi\n");
-  		}
+				memmove(tempStr, tempStr+1, strlen(tempStr));
+	        	printf("%s dereferenced: %s\n", tempStr, getenv(tempStr));
+  			}
+			else if(*(tokens->items[i]) == '~')
+			{
+				strcpy(tempStr, tokens->items[i]);
+				memmove(tempStr, tempStr+1, strlen(tempStr));
+				printf("%s%s\n", getenv("HOME"), tempStr);
+			}
 		}
 
 		free(input);
