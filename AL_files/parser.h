69f9d3da906e51e1c7fe0aa782521efa3b92af2a
11drew12314 <54300964+11drew12314@users.noreply.github.com>
2020-09-13 19:32:02 -0400

    Update parser.h
    add prototypes for 2 methods in piping.c

--- AL_files/parser.h
+++ AL_files/parser.h
@@ -8,3 +8,6 @@ void add_token(tokenlist *tokens, char *item);
 char *get_input(void);
 tokenlist *get_tokens(char *input);
 void free_tokens(tokenlist *tokens);
+
+tokenlist** partitionPipes(tokenlist* cmdStr);
+char* strsep(char** cmdStr, const char* delim);
